d6db8d50e8e611d311c3db3324a44eda2ddb339b
avallon <makeevvolodja@gmail.com>
2017-09-06 23:14:45 +0300

    Task1

--- tasks/linked_lists/module.c
+++ tasks/linked_lists/module.c
@@ -4,69 +4,104 @@
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/errno.h>
+#include <linux/sched/signal.h>
 
 #include "stack.h"
 #include "assert.h"
 
 static int __init test_stack(void)
 {
-    int ret = 0;
-    LIST_HEAD(data_stack);
-    stack_entry_t *tos = NULL;
-    const char *tos_data = NULL;
-    const char* test_data[] = { "1", "2", "3", "4" };
-    long i = 0;
+	int ret = 0;
+	LIST_HEAD(data_stack);
+	stack_entry_t *tos = NULL;
+	const char *tos_data = NULL;
+	const char* test_data[] = { "1", "2", "3", "4" };
+	long i = 0;
 
-    pr_alert("Testing basic stack");
+	pr_alert("Testing basic stack");
 
-    for (i = 0; i != ARRAY_SIZE(test_data); ++i) {
-        tos = create_stack_entry((void*)test_data[i]);
-        if (!tos) {
-            ret = -ENOMEM;
-            break;
-        }
-        stack_push(&data_stack, tos);
-    }
+	for (i = 0; i != ARRAY_SIZE(test_data); ++i) {
+		tos = create_stack_entry((void*)test_data[i]);
+		if (!tos) {
+			ret = -ENOMEM;
+			break;
+		}
+		stack_push(&data_stack, tos);
+	}
 
-    for (i = ARRAY_SIZE(test_data) - 1; i >= 0; --i) {
-        tos = stack_pop(&data_stack);
-        tos_data = STACK_ENTRY_DATA(tos, const char*);
-        delete_stack_entry(tos);
-        printk(KERN_ALERT "%s == %s\n", tos_data, test_data[i]);
-        assert(!strcmp(tos_data, test_data[i]));
-    }
+	for (i = ARRAY_SIZE(test_data) - 1; i >= 0; --i) {
+		tos = stack_pop(&data_stack);
+		tos_data = STACK_ENTRY_DATA(tos, const char*);
+		delete_stack_entry(tos);
+		printk(KERN_ALERT "%s == %s\n", tos_data, test_data[i]);
+		assert(!strcmp(tos_data, test_data[i]));
+	}
 
-    assert(stack_empty(&data_stack));
-    if (ret == 0)
-        pr_alert("Test success!\n");
+	assert(stack_empty(&data_stack));
+	if (ret == 0)
+		pr_alert("Test success!\n");
 
-    return ret;
+	return ret;
 }
 
 static int __init print_processes_backwards(void)
 {
-    // TODO
-    return -ENODEV;
+	int ret = 0;
+	LIST_HEAD(fname_stack); // stack of copied names
+	stack_entry_t *tos = NULL;
+	const char* comm_buf = NULL;
+	struct task_struct *p = NULL;
+
+	for_each_process(p) {
+		comm_buf = kmalloc(sizeof(p->comm), GFP_KERNEL);
+		if (!comm_buf) {
+			ret = -ENOMEM;
+			break;
+		}
+		get_task_comm((char*)comm_buf, p);
+		tos = create_stack_entry((void*)comm_buf);
+		if (!tos) {
+			ret = -ENOMEM;
+			kfree(comm_buf);
+			break;
+		}
+		stack_push(&fname_stack, tos);
+	}
+
+	while (!stack_empty(&fname_stack)) {
+		tos = stack_pop(&fname_stack);
+		comm_buf = STACK_ENTRY_DATA(tos, const char*);
+		printk(KERN_DEFAULT "%s\n", comm_buf);
+		kfree(comm_buf);
+		delete_stack_entry(tos);
+	}
+
+	assert(stack_empty(&fname_stack));
+
+	if (ret == 0)
+		pr_alert("Processes printed!\n");
+
+	return ret;
 }
 
 static int __init ll_init(void)
 {
-    int ret = 0;
-    printk(KERN_ALERT "Hello, linked_lists\n");
+	int ret = 0;
+	printk(KERN_ALERT "Hello, linked_lists\n");
 
-    ret = test_stack();
-    if (ret)
-        goto error;
+	ret = test_stack();
+	if (ret)
+		goto error;
 
-    ret = print_processes_backwards();
+	ret = print_processes_backwards();
 
 error:
-    return ret;
+	return ret;
 }
 
 static void __exit ll_exit(void)
 {
-    printk(KERN_ALERT "Goodbye, linked_lists!\n");
+	printk(KERN_ALERT "Goodbye, linked_lists!\n");
 }
 
 module_init(ll_init);
--- tasks/linked_lists/stack.c
+++ tasks/linked_lists/stack.c
@@ -1,28 +1,42 @@
 #include "stack.h"
+#include "assert.h"
+#include <linux/slab.h>
+#include <linux/gfp.h>
 
 stack_entry_t* create_stack_entry(void *data)
 {
-    // TODO
-    (void)data;
-    return NULL;
+	stack_entry_t* entry = NULL;
+	assert(data);
+
+	entry = kmalloc(sizeof(stack_entry_t), GFP_KERNEL);
+	if (entry) {
+		entry->data = data;
+		return entry;
+	}
+
+	return NULL;
 }
 
 void delete_stack_entry(stack_entry_t *entry)
 {
-    // TODO
-    (void)entry;
+	assert(entry);
+	kfree(entry);
 }
 
 void stack_push(struct list_head *stack, stack_entry_t *entry)
 {
-    // TODO
-    (void)stack;
-    (void)entry;
+	assert(stack);
+	assert(entry);
+	list_add_tail(&entry->lh, stack);
 }
 
 stack_entry_t* stack_pop(struct list_head *stack)
 {
-    // TODO
-    (void)stack;
-    return NULL;
+	assert(stack);
+	if (!list_empty(stack)) {
+		stack_entry_t* res = list_entry(stack->prev, stack_entry_t, lh);
+		list_del(stack->prev);
+		return res;
+	}
+	return NULL;
 }
